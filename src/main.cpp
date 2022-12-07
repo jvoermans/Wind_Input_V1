6c934d3cb1a8d7abb8649f5ab8accfc2a7aded28
jvoermans <43016672+jvoermans@users.noreply.github.com>
2022-12-08 10:24:28 +1100

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <SPI.h>
+#include <BMP388_DEV.h>                           // Include the BMP388_DEV.h library, pressure sensor
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
