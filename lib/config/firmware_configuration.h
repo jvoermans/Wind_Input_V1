d837dcda27c01cbd6ddecaf2fca307b968369fd7
JRT <jean.rblt@gmail.com>
2023-01-10 14:13:53 +0100

    A weird import bug

--- lib/config/firmware_configuration.h
+++ lib/config/firmware_configuration.h
@@ -1,6 +1,16 @@
 #ifndef FIRMWARE_CONFIGURATION_H
 #define FIRMWARE_CONFIGURATION_H
 
+#include "SdFat.h"
+
+// which kind of card format is used
+// this is what works on my 32 GB SD card
+typedef SdFs sd_t;
+typedef FsFile file_t;
+// may need to use ExFat so that can have large SD cards
+// typedef SdExFat sd_t;
+// typedef ExFile file_t;
+
 #include "Arduino.h"
 #include "macro_utils.h"
 #include "print_utils.h"
--- lib/config/sd_configuration.h
+++ lib/config/sd_configuration.h
@@ -1,14 +1,4 @@
 #ifndef SD_CONFIGURATION_H
 #define SD_CONFIGURATION_H
 
-#include "SdFat.h"
-
-// which kind of card format is used
-// this is what works on my 32 GB SD card
-typedef SdFs sd_t;
-typedef FsFile file_t;
-// may need to use ExFat so that can have large SD cards
-// typedef SdExFat sd_t;
-// typedef ExFile file_t;
-
 #endif
